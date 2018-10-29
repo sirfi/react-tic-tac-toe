429cd225d7d4832414df8f9ed7e4b85309ea944d
Fatih Iþýk <sir.fatih.isik@gmail.com>
2018-10-29 23:37:57 +0300

    Updates

---
+++ precache-manifest.d130f51a1f693e3e2bd8d4c93685bbf8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87d698cc4d3f04a284f2",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.87d698cc.js"
+  },
+  {
+    "revision": "41cfabdd2340bc10d5c0",
+    "url": "/react-tic-tac-toe/static/js/main.41cfabdd.chunk.js"
+  },
+  {
+    "revision": "eb1dcec48f16df02dcef",
+    "url": "/react-tic-tac-toe/static/js/1.eb1dcec4.chunk.js"
+  },
+  {
+    "revision": "41cfabdd2340bc10d5c0",
+    "url": "/react-tic-tac-toe/static/css/main.08a047f0.chunk.css"
+  },
+  {
+    "revision": "31c77901a22ff55ee9b29e8be99d9512",
+    "url": "/react-tic-tac-toe/index.html"
+  }
+];
