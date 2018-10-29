b4eb5d8e8231ddc07061b212ce84a96ab5647248
Fatih Iþýk <sir.fatih.isik@gmail.com>
2018-10-29 23:54:46 +0300

    homepage address

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Square({value, onClick}) {
     return (
-        <button className="square" onClick={onClick}>
+        <button className={"square " + (value?value.toLowerCase():"")} onClick={onClick}>
             {value}
         </button>
     );
