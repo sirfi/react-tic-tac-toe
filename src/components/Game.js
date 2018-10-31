134ae326f9668f25a1ca4496a397923c80b95f94
Fatih Iþýk <sir.fatih.isik@gmail.com>
2018-10-31 23:15:50 +0300

    computer random playing

--- src/components/Game.js
+++ src/components/Game.js
@@ -13,8 +13,8 @@ function calculateWinner(squares) {
         [2, 4, 6],
     ];
     for (let i = 0; i < lines.length; i++) {
-        const [a,b,c] = lines[i];
-        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
             return squares[a];
     }
     return null;
@@ -28,42 +28,67 @@ class Game extends Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
-            stepNumber:0,
-            xIsNext: true
+            stepNumber: 0,
+            nextPlayer: 'X',
+            playerTypes: {
+                X: 'human',
+                O: 'human'
+            }
         };
     }
 
-    handleClick(i){
-        const history  = this.state.history.slice(0, this.state.stepNumber + 1);
-        const current = history[history.length-1];
+    handleSquareClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
         const squares = current.squares.slice();
-        if(calculateWinner(squares) || squares[i])
+        if (calculateWinner(squares) || squares[i])
             return;
-        squares[i]=this.state.xIsNext ? 'X' : 'O';
+        squares[i] = this.state.nextPlayer;
         this.setState({
-            history:history.concat([{
+            history: history.concat([{
                 squares
             }]),
             stepNumber: history.length,
-            xIsNext : !this.state.xIsNext
-        });
+            nextPlayer: this.state.nextPlayer === 'X' ? 'O' : 'X'
+        }, () => this.computerPlay());
     }
 
-    jumpTo(step){
+    computerPlay() {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares;
+        if (this.state.playerTypes[this.state.nextPlayer] === 'computer' && squares.filter(s=>s===null).length>0) {
+            let i;
+            do {
+                i = Math.round(Math.random() * 8);
+            } while (squares[i] !== null);
+            this.handleSquareClick(i);
+        }
+    }
+
+    handleTypeChange(player, type) {
+        const playerTypes = this.state.playerTypes;
+        playerTypes[player] = type;
         this.setState({
-            stepNumber:step,
-            xIsNext:(step % 2) ===0
-        });
+            playerTypes
+        }, () => this.computerPlay());
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            nextPlayer: (step % 2) === 1 ? 'O' : 'X'
+        }, () => this.computerPlay());
     }
 
     render() {
-        const history  = this.state.history;
+        const history = this.state.history;
         const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
-        const moves = history.map((step, move)=>{
+        const moves = history.map((step, move) => {
             const desc = move ?
-                ('Go to move #' + move):
+                ('Go to move #' + move) :
                 'Go to game start';
             return (
                 <li key={move}>
@@ -73,18 +98,34 @@ class Game extends Component {
         });
 
         let status;
-        if(winner)
+        if (winner)
             status = 'Winner : ' + winner;
         else
-            status = 'Next Player : ' + (this.state.xIsNext ? 'X' : 'O');
+            status = 'Next Player : ' + this.state.nextPlayer;
         return (
             <div className="game">
-                <div className="game-board">
-                    <Board squares={current.squares} onClick={(i)=> this.handleClick(i)} />
+                <div className="game-options">
+                    <label htmlFor="XPlayerType">X Player</label>
+                    <select name="XPlayerType" value={this.state.playerTypes.X}
+                            onChange={e => this.handleTypeChange("X", e.target.value)}>
+                        <option value="human">Human</option>
+                        <option value="computer">Computer</option>
+                    </select>
+                    <label htmlFor="OPlayerType">O Player</label>
+                    <select name="OPlayerType" value={this.state.playerTypes.O}
+                            onChange={e => this.handleTypeChange("O", e.target.value)}>
+                        <option value="human">Human</option>
+                        <option value="computer">Computer</option>
+                    </select>
                 </div>
-                <div className="game-info">
-                    <div>{status}</div>
-                    <ol>{moves}</ol>
+                <div className="game-inner">
+                    <div className="game-board">
+                        <Board squares={current.squares} onClick={(i) => this.handleSquareClick(i)}/>
+                    </div>
+                    <div className="game-info">
+                        <div>{status}</div>
+                        <ol>{moves}</ol>
+                    </div>
                 </div>
             </div>
         );
